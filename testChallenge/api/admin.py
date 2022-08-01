dfcbe9c1bb1c0b9ab8c93e6800e55b35f7559d0f
Alexander Perez <alexcyberay@gmail.com>
2022-08-01 01:04:22 -0400

    add requirements installing

--- testChallenge/api/admin.py
+++ testChallenge/api/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Client
+from .models import Room
+from .models import Reserve
+from .models import Payment
+from .models import Factura
 # Register your models here.
 
 admin.site.register(Client)
+admin.site.register(Room)
+admin.site.register(Reserve)
+admin.site.register(Payment)
+admin.site.register(Factura)
